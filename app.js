486d716c348df6a28749713b33eca9ecfbba40dd
Johan Norberg <johan.norberg@academicpositions.se>
2017-11-14 16:53:25 +0100

    features init

---
+++ app.js
@@ -0,0 +1,19 @@
+
+import ClassicEditor from '@ckeditor/ckeditor5-editor-classic/src/classiceditor';
+import Essentials from '@ckeditor/ckeditor5-essentials/src/essentials';
+import Paragraph from '@ckeditor/ckeditor5-paragraph/src/paragraph';
+import Bold from '@ckeditor/ckeditor5-basic-styles/src/bold';
+import Italic from '@ckeditor/ckeditor5-basic-styles/src/italic';
+import EmbedVideo from './plugin/embed.js';
+
+ClassicEditor
+    .create(document.querySelector( '#editor' ), {
+        plugins: [Essentials, Paragraph, Bold, Italic, EmbedVideo],
+        toolbar: ['bold', 'italic', 'embedVideo']
+    })
+    .then(editor => {
+        console.log('Editor was initialized', editor);
+    })
+    .catch(error => {
+        console.error(error.stack);
+    });
---
+++ plugin/embed.js
@@ -0,0 +1,54 @@
+import Plugin from '@ckeditor/ckeditor5-core/src/plugin';
+import ButtonView from '@ckeditor/ckeditor5-ui/src/button/buttonview';
+import videoIcon from './../assets/video-camera.svg';
+import ModelElement from '@ckeditor/ckeditor5-engine/src/model/element';
+
+class EmbedVideo extends Plugin {
+
+    init() {
+
+        const editor = this.editor;
+        
+        editor.ui.componentFactory.add('embedVideo', locale => {
+            
+            const view = new ButtonView(locale);
+            
+            view.set({
+                label: 'Insert video',
+                icon: videoIcon,
+                tooltip: true
+            });
+            
+            view.on('execute', () => {
+
+                const videoUrl = prompt('Video URL');
+                const youtubeRegex = new RegExp('youtube.com');
+
+                if (youtubeRegex.test(videoUrl)) {
+
+                    const youtubeCode = videoUrl.slice(videoUrl.indexOf('?v=') + 3);
+
+                    if (youtubeCode) {
+
+                        editor.document.enqueueChanges(() => {
+                            
+                            const iframeElement = new ModelElement('iframe', {
+                                width: 560,
+                                height: 315,
+                                src: 'https://www.youtube.com/embed/' + youtubeCode
+                            });
+
+                            console.log(iframeElement);
+
+                            editor.data.insertContent(iframeElement, editor.document.selection);
+                        });
+                    }
+                }
+            });
+
+            return view;
+        });
+    }
+}
+
+export default EmbedVideo;
